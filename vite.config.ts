0ad13372763b2edd1dd45afe33f37c9c25c9d3a2
arifiansyah56 <arifiansyah737@gmail.com>
2026-08-29 10:33:47 +0700

    chore: set base path for deployment
    Configure the base URL to '/CV_ARIFIANSYAH/' to ensure correct asset resolution when deploying to GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/CV_ARIFIANSYAH/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
